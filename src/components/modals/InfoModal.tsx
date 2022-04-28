b59749158314532c1c07defc35514902c7af7f57
mrxmy <104512597+mrxmy@users.noreply.github.com>
2022-04-28 11:36:04 +0800

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -14,66 +14,52 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         change to show how close your guess was to the word.
       </p>
 
-      <div className="flex justify-center mb-1 mt-4">
+     <div className="flex justify-center mb-1 mt-4">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="N"
+          value="W"
           status="correct"
         />
+        <Cell value="E" />
         <Cell value="A" />
+        <Cell value="R" />
+        <Cell value="Y" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter W is in the word and in the correct spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="P" />
+        <Cell value="I" />
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="L"
+          status="present"
+        />
+        <Cell value="O" />
         <Cell value="T" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter L is in the word but in the wrong spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="V" />
+        <Cell value="A" />
+        <Cell value="G" />
+        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
         <Cell value="E" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter N is in the word but in the wrong spot. 
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="B"
-          status="correct"
-        />
-        <Cell isRevealing={true} isCompleted={true} value="G" status="absent" />
-        <Cell isRevealing={true} isCompleted={true} value="Y" status="absent" />
-        <Cell isRevealing={true} isCompleted={true} value="O" status="absent" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter B is in the word and in the correct spot
-        The other letters are not in word in any spot
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="B"
-          status="correct"
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="I"
-          status="correct"
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="N"
-          status="correct"
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="I"
-          status="correct"
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The word is correct.
+        The letter U is not in the word in any spot.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
         This is a BGYO & BINI version of wordle game! 
-        -{' '}
+       ㅤ -{' '}
         <a
           href="https://twitter.com/porcallanians"
           className="underline font-bold"
